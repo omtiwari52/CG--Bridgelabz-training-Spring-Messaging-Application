7803938009de97e842628f885b0691ba276a6707
omtiwari52 <tiwariom900@gmail.com>
2025-02-21 22:39:27 +0530

    [Om Tiwari] add: Spring messaging project GET Request Method and pass name as query parameter feature added✨

--- springmessagingapp/src/main/java/com/example/demo/springmessagingapp/controller/MessageRestController.java
+++ springmessagingapp/src/main/java/com/example/demo/springmessagingapp/controller/MessageRestController.java
@@ -2,6 +2,7 @@ package com.example.demo.springmessagingapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,10 @@ public class MessageRestController {
     @GetMapping   //get request to print message
     public String hello() {
         return "Hello from BridgeLabz";
+    }
+
+    @GetMapping("/")
+    public String sayHello(@RequestParam String name) {
+        return "Hello, " + name + " from BridgeLabz!";
     }
 }
